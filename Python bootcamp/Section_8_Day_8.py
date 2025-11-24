2c3feedf2aefb95c93afce8937167e53bd571e9b
Bradley McKenzie <bjmcken@hotmail.com>
2025-11-24 13:29:34 -0500

    adding code to Section_8_Day_8 ceaser cipher and modules

---
+++ Python bootcamp/Section_8_Day_8.py
@@ -0,0 +1,6 @@
+# Function Parameters & Caeser Cipher Project in this project we will import the greet function from the greet function file and also import the ceaser cipher file to use the functions we created there. this is to get used to working with modules and importing code from other files. it is best practice to keep your code organized and modular.
+import Section_8_Greet_Function
+import Section_8_ceaser_cipher
+
+
+
---
+++ Python bootcamp/Section_8_Greet_Function.py
@@ -0,0 +1,32 @@
+# Function that greets the user very simple uses case with no imputs
+def greet():
+    print("Hello! Welcome to the Caeser Cipher program.")
+    print("This program will help you encrypt and decrypt messages using the Caeser Cipher technique.")
+    print("Let's get started!")
+greet()
+
+# Function that allows for inputs you can change the variable to a name you wish. In the def at the end you call the function and pass in the name you want to use. Also the paramter is the name in the def line. and the argument is the name you pass in when you call the function.
+#def greet_with_name(name):
+    #print(f"Hello, {name}! Welcome to the Caeser Cipher program.")
+    #print("This program will help you encrypt and decrypt messages using the Caeser Cipher technique.")
+    #print("Let's get started!")
+#greet_with_name("Brad")
+
+# Function with more than one input
+# name = "Brad"
+# location = "New York"
+# def greet_with(name, location):
+    #print(f"Hello, {name}!")
+    #print(f"What a great day in {location}!")
+# greet_with(name, location)
+
+#postional arguments
+#greet_with("New York", "Brad")  # This will mix up the output
+
+
+
+
+
+
+
+
